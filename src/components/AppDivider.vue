d6d901b60510fc5a17d1837815398a7ccca7cfff
Remi Thebault <remi.thebault@gmail.com>
2021-07-28 23:52:01 +0200

    AppDivider

---
+++ src/components/AppDivider.vue
@@ -0,0 +1,25 @@
+<template>
+  <hr :class="classes" class="border-opacity-25" role="separator" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const horizontalClasses = ["border-t", "w-full", "h-0"];
+    const verticalClasses = ["border-r", "h-full", "w-0"];
+    return {
+      classes: computed(() =>
+        props.vertical ? verticalClasses : horizontalClasses
+      ),
+    };
+  },
+});
+</script>
