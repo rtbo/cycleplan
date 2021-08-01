40a174ac2471cc17b2c926e283a16c57d1845ae6
Remi Thebault <remi.thebault@gmail.com>
2021-08-01 09:23:33 +0200

    color-accent

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     extend: {
       colors: {
         "on-surface": opacityHandler("--color-on-surface"),
+        "accent": opacityHandler("--color-accent"),
       }
     },
   },
