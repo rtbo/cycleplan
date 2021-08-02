9a02fa842aec30075f010ba0aa4bd5cc1ef93bfa
Remi Thebault <remi.thebault@gmail.com>
2021-08-02 08:27:25 +0200

    IconTaskInsert

---
+++ src/components/IconTaskInsert.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    class="fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g>
+      <rect x="0" y="4" width="38" height="14" class="text-primary" />
+      <rect x="0" y="20" width="48" height="8" class="text-secondary" />
+      <rect x="10" y="30" width="38" height="14" class="text-primary" />
+    </g>
+  </svg>
+</template>
