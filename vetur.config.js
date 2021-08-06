6862c9a5aa10fa8388e700ce370401753dbddfa5
Remi Thebault <remi.thebault@gmail.com>
2021-08-06 13:45:10 +0200

    add VTI to lints

---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  settings: {
+    "vetur.experimental.templateInterpolationService": false,
+    "vetur.validation.templateProps": true,
+    "vetur.validation.interpolation": true,
+    "vetur.validation.script": true,
+    "vetur.validation.style": true,
+    "vetur.validation.template": true,
+  },
+  projects: ["./"],
+};
