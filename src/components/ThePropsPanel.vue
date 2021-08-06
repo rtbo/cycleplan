b8ae8caad504fdfacd16c8d09969f1cafac4eed0
Remi Thebault <remi.thebault@gmail.com>
2021-08-06 23:10:27 +0200

    continue props-panel

--- src/components/ThePropsPanel.vue
+++ src/components/ThePropsPanel.vue
@@ -1,15 +1,6 @@
 <template>
-  <div
-    class="
-      h-full
-      p-2
-      bg-on-background bg-opacity-5
-      border-l border-on-background border-opacity-20
-      dark:border-opacity-20
-      props-panel
-    "
-  >
-    <h2 class="text-lg">Properties</h2>
+  <div class="props-panel">
+    <h2 class="props-panel__title">Properties</h2>
     <span v-if="!task" class="italic">No task selected</span>
     <div v-else>
       <div class="props-panel__form">
@@ -48,32 +39,77 @@
           {{ task.slack }}
         </span>
       </div>
-      <app-divider :opacity="30" />
-      <h2 class="text-lg">Starting conditions</h2>
-      <div class="props-panel__form">
-        <label for="props-panel__not-before" class="props-panel__label">
-          Not before
-        </label>
-        <app-click-to-edit
-          id="props-panel__not-before"
-          class="props-panel__field font-bold"
-          v-model="notBefore"
-        />
-      </div>
-      <div v-for="(cond, index) in startingConds" :key="index">
-        <app-divider />
-        <div class="flex flex-row items-center p-2">
-          <div class="flex-grow"></div>
-          <div>
+      <app-divider :opacity="30" class="mt-2" />
+      <h2 class="props-panel__title">Starting conditions</h2>
+      <div class="px-1">
+        <div class="props-panel__form">
+          <label for="props-panel__not-before" class="props-panel__label">
+            Not before
+          </label>
+          <app-click-to-edit
+            id="props-panel__not-before"
+            class="props-panel__field font-bold"
+            v-model="notBefore"
+          />
+        </div>
+        <app-divider class="my-2" />
+        <div v-for="(cond, index) in startingConds" :key="index">
+          <div class="grid grid-cols-12 gap-x-1 gap-y-2">
+            <select
+              class="
+                col-span-4
+                text-sm
+                underline
+                bg-transparent
+                cursor-pointer
+                move-left-1
+              "
+              :value="cond.mode"
+              @change="setLinkMode(cond, $event.target.value)"
+            >
+              <option value="with" class="add-colon">With</option>
+              <option value="after" class="add-colon">After</option>
+            </select>
+            <select
+              class="col-span-8 font-semibold bg-transparent"
+              :value="cond.taskFromId"
+            >
+              <option
+                v-for="task in possibleTaskFrom(cond.taskFromId)"
+                :key="task.id"
+                :value="task.id"
+              >
+                {{ task.name }}
+              </option>
+            </select>
+            <span class="text-sm underline col-span-4 add-colon"> Lag </span>
+            <app-click-to-edit
+              class="col-span-6 font-semibold"
+              input-class="font-semibold"
+              :model-value="cond.lag"
+              @update:model-value="setLinkLag(cond, value)"
+              :validate="isNumber"
+            />
             <app-button
-              class="bg-red-600 dark:bg-red-400 text-on-error mdi mdi-trash-can"
+              class="
+                col-span-2
+                bg-red-600
+                dark:bg-red-400
+                text-on-error
+                mdi mdi-trash-can
+              "
               @click="deleteStartingCond(cond.taskFromId)"
             ></app-button>
           </div>
+          <app-divider class="my-2" />
         </div>
-      </div>
-      <div class="flex flex-row justify-end">
-        <app-icon-button class="mx-3 text-xl" icon="mdi-plus"></app-icon-button>
+        <div class="flex flex-row">
+          <div class="flex-grow"></div>
+          <app-icon-button
+            class="mx-2 text-xl"
+            icon="mdi-plus"
+          ></app-icon-button>
+        </div>
       </div>
     </div>
   </div>
@@ -82,7 +118,7 @@
 <script lang="ts">
 import { computed, ComputedRef, defineComponent } from "vue";
 import { useStore } from "@/store";
-import { isPositiveNumber } from "@/util";
+import { isPositiveNumber, isNumber } from "@/util";
 
 interface StartingCond {
   mode: "with" | "after";
@@ -136,6 +172,42 @@ export default defineComponent({
       }))
     );
 
+    const taskFromIds = computed(() =>
+      startingConds.value.map((sc) => sc.taskFromId)
+    );
+
+    const possibleTaskFrom = (currTaskFrom: number | undefined) => {
+      if (!task.value) return [];
+      return store.state.tasks.filter(
+        (t) =>
+          (t.id === currTaskFrom || !taskFromIds.value.includes(t.id)) &&
+          t.id !== task.value?.id
+      );
+    };
+
+    const setLinkMode = (cond: StartingCond, mode: "with" | "after") => {
+      if (!task.value) return;
+
+      store.commit("update-link", {
+        from: cond.taskFromId,
+        to: task.value.id,
+        fromStart: mode === "with",
+      });
+    };
+
+    const setLinkLag = (cond: StartingCond, lag: string) => {
+      if (!task.value) return;
+
+      const lagVal = parseFloat(lag.replace(",", "."));
+      if (!isNaN(lagVal)) {
+        store.commit("update-link", {
+          from: cond.taskFromId,
+          to: task.value.id,
+          lag: lagVal,
+        });
+      }
+    };
+
     const deleteStartingCond = (taskFromId: number) => {
       if (task.value)
         store.commit("delete-link", { from: taskFromId, to: task.value.id });
@@ -147,8 +219,12 @@ export default defineComponent({
       notBefore,
       duration,
       startingConds,
+      possibleTaskFrom,
+      setLinkMode,
+      setLinkLag,
       deleteStartingCond,
       isPositiveNumber,
+      isNumber,
     };
   },
 });
@@ -156,14 +232,26 @@ export default defineComponent({
 
 <style lang="postcss">
 .props-panel {
+  @apply h-full p-2 bg-on-background bg-opacity-5 border-l border-on-background border-opacity-20 dark:border-opacity-20;
   box-shadow: -5px 0px 10px -2px rgba(var(--color-on-background), 0.1);
 }
-.props-panel__form {
-  @apply grid grid-cols-12 items-center gap-1 my-2;
+
+.props-panel__title {
+  @apply text-lg my-2;
 }
+
+.props-panel__form {
+  @apply grid grid-cols-12 items-center gap-1;
+}
+
 .props-panel__label {
   @apply text-sm underline col-span-4;
+  @apply add-colon;
 }
+
+/* .props-panel__label::after {
+} */
+
 .props-panel__field {
   @apply col-span-8;
 }
